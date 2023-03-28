26cdc20740646c1d3e821835cf33e97a03c32888
Geovane <geovane.piccinin@gmail.com>
2023-03-28 16:04:09 -0300

    installing firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAa1Mq9Jw_ZnwxvO005k2OtJVDT6tYOKxA",
+  authDomain: "super-list-firebase.firebaseapp.com",
+  projectId: "super-list-firebase",
+  storageBucket: "super-list-firebase.appspot.com",
+  messagingSenderId: "598212174719",
+  appId: "1:598212174719:web:2516855fbebdb0263cb04a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
